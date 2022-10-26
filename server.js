2a1f6200d1b280d0a18e81bc6fb87f01b7c77c7b
Brit Sovic <bsovic@tulane.edu>
2022-10-26 11:21:30 -0400

    parties table

--- server.js
+++ server.js
@@ -11,17 +11,16 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Brscuse10111994!!',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
-
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
@@ -39,7 +38,6 @@ app.get('/api/candidates', (req, res) => {
   });
 });
 
-
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
   const sql = `SELECT * FROM candidates WHERE id = ?`;
@@ -56,7 +54,6 @@ app.get('/api/candidate/:id', (req, res) => {
     });
   });
 });
-
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -80,28 +77,33 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 });
 
-
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message });
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: body
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
   });
-});
 });
 
 // Default response for any other request (Not Found)
